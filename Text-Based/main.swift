fb63a7fcc9f7e3250e65e961930ca9c043e234a1
brett-buwalda <brett-buwalda@codermerlin.codermerlin.com>
2022-05-04 10:08:47 -0500

    main.swift

---
+++ Text-Based/main.swift
@@ -0,0 +1,135 @@
+print("What is the most stolen food in the world?")
+
+let answer1 = readLine()
+
+if let answer1 = answer1 {
+    if answer1 == "Cheese" {
+        print("good job")
+    }
+    else {
+        print(" You're wrong. The correct answer was 'Cheese' ")
+    }
+}
+else {
+    print(" how do you manage to not put in an answer if there's no time limit? ")
+}
+
+print(" Coca-Cola was first sold in which state? ")
+
+let answer2 = readLine()
+
+if let answer2 = answer2 {
+    if answer2 == "Georgia" {
+        print("you know your stuff")
+    }
+    else {
+        print(" you do not know your stuff. The correct answer was 'Georgia' ")
+    }
+}
+else {
+    print("how do you manage to not put in an answer if there's no time limit?")
+}
+
+print("What is the only vitamin not found in an egg?")
+
+let answer3 = readLine()
+
+if let answer3 = answer3 {
+    if answer3 == "Vitamin C" {
+        print(" holy guacamole you're good at this")
+    }
+    else {
+        print("I don't blame you for not knowing this. The right answer was 'Vitamin C")
+    }
+}
+
+print("What dish is also known as bean curd?")
+
+let answer4 = readLine()
+
+if let answer4 = answer4 {
+    if answer4 == "Tofu" {
+        print(" they look like little pillows! good answer")
+    }
+    else {
+        print(" lol i wouldn't know either. The right answer was 'Tofu'")
+    }
+}
+
+print("Which state consumes the most turkey on Thanksgiving day?")
+
+let answer5 = readLine()
+
+if let answer5 = answer5 {
+    if answer5 == "California" {
+        print("It's kind of obvious, wouldn't you think? Nonetheless good answer")
+    }
+    else {
+        print("bro, it's literally California. The state with the biggest population")
+    }
+}
+
+print("What is considered to be the oldest vegetable?")
+
+let answer6 = readLine()
+
+if let answer6 = answer6 {
+    if answer6 == "Pea" {
+        print("peas are green, just like your backpack probably. It was off-topic but hey, you got the answer right so good work")
+    }
+    else {
+        print("peas are round and squishy. The answer was 'Pea'")
+    }
+}
+
+print("Approximately, how many seeds are in a strawberry?")
+
+let answer7 = readLine()
+
+if let answer7 = answer7 {
+    if answer7 == "200" {
+        print("strawberries do have 200 seeds per. Unless you eat them. Then they're dead. Good job though")
+    }
+    else {
+        print("haha you don't know how many seeds are in a strawberry. The answer is '200'")
+    }
+}
+
+print("What were chicken nuggets called when they were first invented?")
+
+let answer8 = readLine()
+
+if let answer8 = answer8 {
+    if answer8 == "Chicken Crispies" {
+        print("Chicken Crispies are really good. amazing work on this answer")
+    }
+    else {
+        print(" I can't really be disappointed at you. You've known them as chicken tenders and chicken strips your whole life. The answer was 'Chicken Crispies'")
+    }
+}
+
+print("Which is the only edible food that never expires?")
+
+let answer9 = readLine()
+
+if let answer9 = answer9 {
+    if answer9 == "Honey" {
+        print("correct. Honey is really tasty too so that makes it twice as good")
+    }
+    else {
+        print("Dang, this is like one of the only foods that never go bad. The answer was 'Honey'")
+    }
+}
+
+print(" Percentage-wise, how much of a Twinkie consists of air?")
+
+let answer10 = readLine()
+
+if let answer10 = answer10 {
+    if answer10 == "68%" {
+        print(" That was either a lucky guess, you googled it, or you're just really knowledgeable about Twinkies")
+    }
+    else {
+        print(" Can't be smart if you don't know this answer. The answer was '68%'")
+    }
+}
